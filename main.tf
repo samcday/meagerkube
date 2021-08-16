a4cb4138c97eef4ff5b4647595aac69ce9ca9b64
Sam Day <me@samcday.com>
2021-08-16 14:12:42 +0200

    add a network + subnet, include SSH in firewall rules

--- main.tf
+++ main.tf
@@ -27,17 +27,28 @@ provider "hcloud" {
 
 resource "hcloud_firewall" "firewall" {
   name = "firewall"
-  rule {
-    direction = "in"
-    protocol = "tcp"
-    port = "6443"
-    description = "kube-apiserver"
-    source_ips = ["0.0.0.0/0", "::/0"]
+  labels = {}
+
+  dynamic "rule" {
+    for_each = {"22": "ssh", "443": "https", "6443": "kube-apiserver"}
+    content {
+      direction = "in"
+      protocol = "tcp"
+      port = rule.key
+      description = rule.value
+      source_ips = ["0.0.0.0/0", "::/0"]
+    }
   }
-  rule {
-    direction = "in"
-    protocol = "tcp"
-    port = "443"
-    source_ips = ["0.0.0.0/0", "::/0"]
-  }
+}
+
+resource "hcloud_network" "network" {
+  name = "network"
+  ip_range = "10.240.0.0/13"
+}
+
+resource "hcloud_network_subnet" "subnet-nodes" {
+  network_id = hcloud_network.network.id
+  type = "cloud"
+  network_zone = "eu-central"
+  ip_range = "10.240.0.0/16"
 }
