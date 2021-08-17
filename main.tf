7c27cb8f6c45c00620f6eb42833b5fd1d1f85f9d
Sam Day <me@samcday.com>
2021-08-17 08:44:44 +0200

    conditionals r hard

--- main.tf
+++ main.tf
@@ -181,7 +181,7 @@ resource "null_resource" "kubeadm-join" {
 
   provisioner "remote-exec" {
     inline = [
-      "[[ ! -f /etc/kubernetes/kubelet.conf ]] && kubeadm join --config /root/kubeadm.yaml"
+      "if [[ ! -f /etc/kubernetes/kubelet.conf ]]; then kubeadm join --config /root/kubeadm.yaml; fi"
     ]
   }
 }
